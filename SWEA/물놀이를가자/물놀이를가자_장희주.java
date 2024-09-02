a0e0f52e720630748dc78545430ee8ff938e43c1
jang-huiju <gmlwn6316@gmail.com>
2024-09-02 14:45:14 +0900

    물놀이를가자_장희주

--- SWEA/물놀이를가자/물놀이를가자_장희주.java
+++ SWEA/물놀이를가자/물놀이를가자_장희주.java
@@ -19,13 +19,13 @@ public class 물놀이를가자_장희주 {
 
 	}
 
-	static int n, m,cnt,result;
-	static int totalMin;
+	static int n, m, result;
 	static char[][] map;
 	// 상하좌우
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
-	static boolean[][] visited;
+	static int[][] visited;
+	static Queue<Node> q;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
@@ -39,57 +39,67 @@ public class 물놀이를가자_장희주 {
 			m = Integer.parseInt(st.nextToken());
 			// 맵정보 W:물 L:땅
 			map = new char[n][m];
+			//방문처리
+			visited = new int[n][m];
+			//큐생성
+			q = new ArrayDeque<>();
 			for (int i = 0; i < n; i++) {
 				String str = br.readLine();
 				for (int j = 0; j < m; j++) {
+					//맵정보 저장
 					map[i][j] = str.charAt(j);
-				}
-			}
-			cnt=0;
-			result=0;
-			totalMin=Integer.MAX_VALUE;
-			visited=new boolean[n][m];
-			// 함수실행
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (map[i][j] == 'L') {
-						bfs(i, j);
-						result+=totalMin;
+					//물일때 위치 좌표 저장
+					if (map[i][j] == 'W') {
+						q.add(new Node(i, j));
+						//방문처리 해줌
+						visited[i][j] = 0;
+					//물이 아닐때 -1 로 초기화
+					} else {
+						visited[i][j] = -1;
 					}
 				}
 			}
+			//결과값
+			result = 0;
+			bfs();
+			//거리 최소 값 더하기
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					result += visited[i][j];
+				}
+			}
+			// 함수실행
 			System.out.println("#" + test_case + " " + result);
 		}
 
 	}
-
-	static void bfs(int x, int y) {
-		Queue<Node> q = new ArrayDeque<>();
-		q.add(new Node(x, y));
-		visited=new boolean[n][m];
-		visited[x][y]=true;
+	
+	static void bfs() {
+		
+		//큐가 비어질때까지
 		while (!q.isEmpty()) {
+			//큐에 위치좌표 꺼내기
 			Node node = q.poll();
-			int curX = node.x;
-			int curY = node.y;
+			int x = node.x;
+			int y = node.y;
+			//4방향 돌기
 			for (int i = 0; i < 4; i++) {
-				int nx = curX + dx[i];
-				int ny = curY + dy[i];
-				if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				//범위에 벗어나면
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
 					continue;
 				}
-				if (!visited[nx][ny] && map[nx][ny]=='W') {
-					visited[nx][ny]=true;
-					curX = nx;
-					curY = ny;
-					q.offer(new Node(nx,ny));
-					cnt++;
+				//방문하지않았다면
+				if (visited[nx][ny] == -1) {
+					//전에 있는 값에 +1 더하기
+					visited[nx][ny] = visited[x][y] + 1;
+					//큐에 삽입
+					q.add(new Node(nx, ny));
 				}
+
 			}
-
 		}
-		
-
 	}
 
 }
