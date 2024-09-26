d31f1524b840e7b1bedd1db4ef013e2c54a2d96f
PikaYeti <166683154+PikaYeti@users.noreply.github.com>
2024-09-26 10:00:57 +0900

    수정  0926_Main_14501_김가연.java

--- src/BJ/Main_14501_김가연.java
+++ src/BJ/Main_14501_김가연.java
@@ -1,75 +1,94 @@
+package D0925;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_14501_김가연 {
-
-    static int [][] arr;
-    static int max;
-
+	
+	static int arr [][];
+	static int max;
+	
 	static void next(int cnt, int idx, int n) {
-	    
-	    int ccnt = 0 ;
-	    
-	    for (int i = 1 ; i < n - idx ; i++) {
-	        if (idx + arr[idx + i][0] < n) {
-	        	if (arr[idx + i][0] + idx + i <= n) {
-	        		ccnt = cnt + arr[idx + i][1];
-		            
-		            idx = idx + arr[idx + i][0];
+		
+		if (idx > n) {
+			return;
+		}
+		
+		for (int i = 0 ; i < n - idx ; i ++) {
+			
+	        int ccnt = 0;
+	        int cidx = 0;
+	        
+	        cidx = idx + (arr[idx][0] + i);
+
+	        if (cidx < n) {
+	        	if (arr[cidx][0] + cidx <= n) {
+	        		
+	        		ccnt = cnt + arr[cidx][1];
 		            
 		            if (ccnt > max) {
 		                max = ccnt;
 		            }
 		            
-		            next(ccnt, idx, n);
+		            next(ccnt, cidx, n);
+
 	        	}
+	            
 	        }
 	    }
+		
 	}
-	 
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	
+		
 	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	
 	    int n = Integer.parseInt(bf.readLine());
 	
 	    arr = new int [n][2];
+	    
+	    max = Integer.MIN_VALUE;
 	
 	    for (int i = 0 ; i < n ; i++) {
 	        StringTokenizer st = new StringTokenizer(bf.readLine());
 	
 	        arr[i][0] = Integer.parseInt(st.nextToken());
 	        arr[i][1] = Integer.parseInt(st.nextToken());
-	    }
-	
-	    int [] save = new int [n];
-	    max = Integer.MIN_VALUE;
-	
-	    for (int i = 0 ; i < n ; i ++) {
-	        int cnt = 0;
-	        int idx = 0;
-	        if (i + arr[i][0] < n) {
-	        	if (arr[i + arr[i][0]][0] + i + arr[i][0] <= n) {
-	        		cnt = arr[i][1] + arr[i + arr[i][0]][1];
-		            
-		            idx = i + arr[i][0];
-		            
-		            if (cnt > max) {
-		                max = cnt;
-		            }
-		            
-		            System.out.println(cnt);
-		            System.out.println(idx);
-		            
-		            next(cnt, idx, n);
-	        	}
-	            
+	        
+	        if ((i + arr[i][0] <= n) && (arr[i][1] > max)) {
+	        	max = arr[i][1];
 	        }
+ 	    }
+	    
+	    for (int i = 0 ; i < n ; i ++) {
+	    	for (int j = 0 ; j < n - i ; j ++) {
+	    		
+	    		int cnt = 0;
+		        int idx = i + arr[i][0] + j;
+		        if (idx < n) {
+		        	if (arr[idx][0] + idx <= n) {
+		        		
+		        		cnt = arr[i][1] + arr[idx][1];
+			            
+			            if (cnt > max) {
+			                max = cnt;
+			            }
+			            
+			            next(cnt, idx, n);
+			            
+		        	}
+		            
+		        }
+	    	}
+	    }
+	    
+	    if (max == Integer.MIN_VALUE) {
+	    	max = 0;
 	    }
 	    
 	    System.out.println(max);
 	}
+
 }
